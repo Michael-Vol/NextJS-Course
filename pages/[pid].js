f5bd60fd004765218b77d8b7fb9df6d69fe9dbb8
Michael-Vol <mivolakis@gmail.com>
2021-11-01 00:18:03 +0200

    working with fallback pages

--- pages/[pid].js
+++ pages/[pid].js
@@ -3,6 +3,10 @@ import path from 'path';
 import { Fragment } from 'react';
 function ProductDetailPage(props) {
 	const { loadedProduct } = props;
+
+	if (!loadedProduct) {
+		return <p>Loading...</p>;
+	}
 	return (
 		<Fragment>
 			<h1>{loadedProduct.title}</h1>
@@ -30,8 +34,8 @@ export async function getStaticProps(context) {
 
 export async function getStaticPaths() {
 	return {
-		paths: [{ params: { pid: 'p1' } }, { params: { pid: 'p2' } }, { params: { pid: 'p3' } }],
-		fallback: false,
+		paths: [{ params: { pid: 'p1' } }],
+		fallback: true,
 	};
 }
 
