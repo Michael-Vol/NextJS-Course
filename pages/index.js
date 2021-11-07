ef791ef4ebc12dc1679ab6cb48feda8530602bcc
Michael-Vol <mivolakis@gmail.com>
2021-11-07 02:19:41 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,14 @@
-import fs from 'fs/promises';
-import path from 'path';
-import Link from 'next/link';
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-function HomePage(props) {
-	const { products } = props;
-	return (
-		<ul>
-			{products.map((product) => (
-				<li key={product.id}>
-					<Link href={`/${product.id}`}>{product.title}</Link>
-				</li>
-			))}
-		</ul>
-	);
-}
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
 
-export async function getStaticProps(context) {
-	console.log('Regenerating...');
-	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-	const jsonData = await fs.readFile(filePath);
-	const data = JSON.parse(jsonData);
-
-	if (data.products.length === 0) {
-		return {
-			notFound: true,
-		};
-	}
-
-	if (!data) {
-		return {
-			redirect: {
-				destination: '/no-data',
-			},
-		};
-	}
-
-	return {
-		props: {
-			products: data.products,
-		},
-		revalidate: 10,
-	};
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
 
 export default HomePage;
