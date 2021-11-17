95d721676520359b39ad18927ef774cc0ccd9d72
Michael-Vol <mivolakis@gmail.com>
2021-11-18 01:02:50 +0200

    running mongodb queries from inside api routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
 	if (req.method === 'POST') {
 		const userEmail = req.body.email;
 
@@ -7,6 +9,15 @@ function handler(req, res) {
 				message: 'Invalid Email Address.',
 			});
 		}
+
+		const client = await MongoClient.connect(
+			`mongodb+srv://admin:rimdus-cuxquS-1hobro@cluster0.sq1ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+		);
+		const db = client.db();
+
+		await db.collection('emails').insertOne({ email: userEmail });
+
+		client.close();
 
 		console.log(userEmail);
 		res.status(201).json({ message: 'Signed up!' });
