61c5a12fbd960155c6609bd59af2d5578545c456
Michael-Vol <mivolakis@gmail.com>
2021-11-24 00:29:49 +0200

    starting work on the "Featured Posts" part

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,11 @@
+import classes from './featured-posts.module.css';
+
+function featuredPosts() {
+	return (
+		<section className={classes.latest}>
+			<h2>Featured Posts</h2>
+		</section>
+	);
+}
+
+export default featuredPosts;
