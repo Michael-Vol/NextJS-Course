a06822ebd8d5dafbb34839fbcd440ec85b9e3058
Michael-Vol <mivolakis@gmail.com>
2021-11-24 02:02:21 +0200

    rendering dummy post data

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,23 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import classes from './posts-item.module.css';
+import classes from './post-item.module.css';
 
 function PostItem(props) {
 	const { title, image, excerpt, date, slug } = props.post;
 
 	const imagePath = `/images/posts/${slug}/${image}`;
+	const linkPath = `/posts/${slug}`;
 	const formattedDate = new Date(date).toLocaleDateString('en-US', {
 		day: 'numeric',
 		month: 'long',
 		year: 'numeric',
 	});
 	return (
-		<li>
-			<Link>
+		<li className={classes.post}>
+			<Link href={linkPath}>
 				<a>
 					<div className={classes.image}>
-						<Image src='' alt={title} width={300} height={200} />
+						<Image src={imagePath} alt={title} width={300} height={200} layout='responsive' />
 					</div>
 					<div className={classes.content}>
 						<h3>{title}</h3>
