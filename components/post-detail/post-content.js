d1f36fb13617912de0babfee046b4b10ea08d895
Michael-Vol <mivolakis@gmail.com>
2021-11-25 11:40:47 +0200

    rendering code snippets from markdown

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,8 +1,10 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import classes from './post-content.module.css';
 import PostHeader from './post-header';
-
 function PostContent(props) {
 	const { post } = props;
 	const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -36,6 +38,11 @@ function PostContent(props) {
 				);
 			}
 			return <p>{paragraph.children}</p>;
+		},
+		code(code) {
+			const { className, children } = code;
+			const language = className.split('-')[1];
+			return <SyntaxHighlighter language={language} style={atomDark} children={children} />;
 		},
 	};
 
