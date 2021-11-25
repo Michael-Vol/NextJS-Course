070db41438edd435f213f51e9719a8582087ae58
Michael-Vol <mivolakis@gmail.com>
2021-11-25 11:48:59 +0200

    preparing the contact form

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -16,7 +16,7 @@ function MainNavigation() {
 						<Link href='/posts'>Posts</Link>
 					</li>
 					<li>
-						<Link href='/posts'>Contact</Link>
+						<Link href='/contact'>Contact</Link>
 					</li>
 				</ul>
 			</nav>
