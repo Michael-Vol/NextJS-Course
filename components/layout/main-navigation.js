654a118de14ad370ced678c8ef125a859d09c4b3
Michael-Vol <mivolakis@gmail.com>
2021-11-29 19:11:24 +0200

    managing active session on the frontend

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,30 +1,39 @@
 import Link from 'next/link';
-
 import classes from './main-navigation.module.css';
+import { useSession } from 'next-auth/client';
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <Link href='/'>
-        <a>
-          <div className={classes.logo}>Next Auth</div>
-        </a>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const [session, loading] = useSession();
+
+	console.log(loading);
+	console.log(session);
+
+	return (
+		<header className={classes.header}>
+			<Link href='/'>
+				<a>
+					<div className={classes.logo}>Next Auth</div>
+				</a>
+			</Link>
+			<nav>
+				<ul>
+					{!session && !loading && (
+						<li>
+							<Link href='/auth'>Login</Link>
+						</li>
+					)}
+					{session && (
+						<li>
+							<Link href='/profile'>Profile</Link>
+						</li>
+					)}
+					<li>
+						<button>Logout</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default MainNavigation;
